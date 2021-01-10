53de45f55ecafd48babb43b9f0eb13ac3feb18b9
ljl22182 <2218206776@qq.com>
2021-01-10 19:45:49 +0800

    user

---
+++ src/User.java
@@ -0,0 +1,10 @@
+import java.nio.file.attribute.UserPrincipalLookupService;
+
+public class User {
+    private String password;
+    public User(String password){
+        this.password = password;
+    }
+    public int getId(){return id ;}
+    public void setId(int id){}
+}
